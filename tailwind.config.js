00ff719679fa0de6ab02c0ed7254ae1d4c0392dc
cptburah <cptburah@gmail.com>
2024-02-08 16:07:48 +0400

    font-reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,7 +64,7 @@ export default {
                 esnaad_text: '#000000',
             },
             fontFamily: {
-                body: ['Montserrat', 'sans-serif'],
+                body: ['Optima LT W02 Roman'],
             },
             backgroundImage: {
                 'hero-pattern': "url('/public/front/images/hero/bg-image.jpg')",
