0a06260604d0a65da75be751251a3dc50dd30c4c
cptburah <cptburah@gmail.com>
2024-02-13 16:38:42 +0400

    whawtsapp

--- resources/views/layoutv2/master.blade.php
+++ resources/views/layoutv2/master.blade.php
@@ -280,7 +280,6 @@
                 }
             }
         ?>
-        <?php?>
 
         <!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDTQRDXP"
