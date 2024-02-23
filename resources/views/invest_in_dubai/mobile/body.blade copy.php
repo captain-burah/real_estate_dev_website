4af513f2c0925b481604a54d1b605cb177a9576c
cptburah <cptburah@gmail.com>
2024-02-23 12:53:25 +0400

    amobile invest in dubai

--- resources/views/invest_in_dubai/mobile/body.blade copy.php
+++ resources/views/invest_in_dubai/mobile/body.blade copy.php
@@ -137,4 +137,56 @@
     
     </div>
     
+    <div class="mx-auto my-8">
+        
+
+        <h2 class="font-bold leading-8 text-base mt-5 text-justify">
+            {{__('frontend.investH3')}}
+        </h2>
+        <p class=" font-thin leading-8 text-base  text-left">
+            {{__('frontend.investP3')}}
+        </p>
+
+        <h2 class="font-bold leading-8 text-base mt-5 text-justify">
+            {{__('frontend.investH4')}}
+        </h2>
+        <p class=" font-thin leading-8 text-base  text-left">
+            {{__('frontend.investP4')}}
+        </p>
+
+        <h2 class="font-bold leading-8 text-base mt-5 text-justify">
+            {{__('frontend.investH5')}}
+        </h2>
+        <p class=" font-thin leading-8 text-base  text-left">
+            {{__('frontend.investP5')}}
+        </p>
+
+        <h2 class="font-bold leading-8 text-base mt-5 text-justify">
+            {{__('frontend.investH6')}}
+        </h2>
+        <p class=" font-thin leading-8 text-base  text-left">
+            {{__('frontend.investP6')}}
+        </p>
+
+        <h2 class="font-bold leading-8 text-base mt-5 text-justify">
+            {{__('frontend.investH7')}}
+        </h2>
+        <p class=" font-thin leading-8 text-base  text-left">
+            {{__('frontend.investP7')}}
+        </p>
+
+        <h2 class="font-bold leading-8 text-base mt-5 text-justify">
+            {{__('frontend.investH8')}}
+        </h2>
+        <p class=" font-thin leading-8 text-base  text-left">
+            {{__('frontend.investP8')}}
+        </p>
+
+        <h2 class="font-bold leading-8 text-base mt-5 text-justify">
+            {{__('frontend.investH9')}}
+        </h2>
+        <p class=" font-thin leading-8 text-base  text-left">
+            {{__('frontend.investP9')}}
+        </p>
+    </div>
 </div>
