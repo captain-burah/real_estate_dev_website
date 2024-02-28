a5531fea8b8731bd467449b8da596c46020052c3
cptburah <cptburah@gmail.com>
2024-02-28 15:35:32 +0400

    construction growth

--- resources/views/constructions/mobile/body.blade.php
+++ resources/views/constructions/mobile/body.blade.php
@@ -16,15 +16,6 @@
             <div class="">
                 <div class="mx-auto px-0 ">
                     <div class="max-w-full cursor-pointer rounded-0 border p-0 shadow">
-                        <div class="row">
-                            <img class="w-full rounded-0 object-cover object-center" src="{{ URL('https://mis.esnaad.com/uploads/construction/'.$id.'/thumbnail/'.$thumbnail) }}"
-                                height="400px"
-                                width="auto"
-                                loading="lazy"
-                                style="height: 400px !important;"
-                                alt="ESNAAD Media Unit - Gallery - {{$title}}"
-                            />
-                        </div>
                         <div class="row my-2 px-4">
                             <div>
                                 <p class="mt-4 pl-4 text-2xl text-center font-thin">
@@ -34,17 +25,22 @@
                                         {{$title}}
                                     @endif
                                 </p>
+
+                                
                                 <div class="mt-2 h-6 relative w-auto rounded-full overflow-hidden">
                                     <div class=" w-full h-full bg-gray-200 absolute "></div>
-                                    <div class=" h-full bg-yellow-400 sm:bg-green-500 absolute" style="width:{{$growth}}%"><div class="h-full w-full mx-auto text-sm text-center pt-1 text-white font-bold"> &nbsp; &nbsp;</div></div>
+                                    <div class=" h-full bg-green-400 sm:bg-green-500 absolute" style="width:{{$growth}}%"><div class="h-full w-full mx-auto text-sm text-center pt-1 text-white font-bold"> &nbsp; &nbsp;</div></div>
                                 </div>
+                                <p class="mt-4 pl-4 text-base text-center font-thin my-auto">
+                                    Progress {{ $growth }} %
+                                </p>
                             </div>
                         </div>
                         <div class="row text-center pb-5 pt-2">
-                            <a href="{{ URL($langSeg.'/constructions/'.$sluglink) }}" 
+                            {{-- <a href="{{ URL($langSeg.'/constructions/'.$sluglink) }}" 
                                 class="bg-white text-black px-15 py-3">
                                 Explore
-                            </a>
+                            </a> --}}
                         </div>
                     </div>
                 </div>
