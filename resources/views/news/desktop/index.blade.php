4fb3f8d989198b5ff23e5c05796c8d90772949eb
cptburah <cptburah@gmail.com>
2024-03-25 11:27:16 +0400

    press-release

--- resources/views/news/desktop/index.blade.php
+++ resources/views/news/desktop/index.blade.php
@@ -16,7 +16,16 @@
         }
     </style>
 @endsection
-
+<?php
+    $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $uri_segments = explode('/', $uri_path);
+    $seg1 = $uri_segments[1];
+    if($seg1 == 'en' || $seg1 == 'ar'){
+        $langSeg = $uri_segments[1];
+    }else{
+        $langSeg = 'en';
+    }
+?>
 @section('content')
     @notmobile
         <section class="my-0">
