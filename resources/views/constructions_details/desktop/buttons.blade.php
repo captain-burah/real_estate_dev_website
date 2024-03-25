26fddd6759f4b4d826a30f5b2c7ae25a77a6f0ff
cptburah <cptburah@gmail.com>
2024-03-25 12:49:15 +0400

    press-release

--- resources/views/constructions_details/desktop/buttons.blade.php
+++ resources/views/constructions_details/desktop/buttons.blade.php
@@ -45,9 +45,9 @@
             <div class="flex flex-col items-start w-full">
                 <div class="p-7 flex items-stretch w-full">
                     <div class="font-semibold text-xl text-black">Share</div>
-                    <svg onclick="modalClose('mymodalcentered-community-share')" class="ml-auto fill-current text-gray-700 w-5 h-5 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
-                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
-                    </svg>
+                    <div onclick="modalClose('mymodalcentered-community-share')" class="ml-auto fill-current text-black w-5 h-5 cursor-pointer">
+                        X
+                    </div>
                 </div>
                 <div class="flex flex-col items-start w-full"  >
                     <div class="px-7  w-full" style="max-height: 40vh;">
