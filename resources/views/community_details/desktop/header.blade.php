6b7dac69d3400037b96140fbdf976ea315efbdb1
cptburah <cptburah@gmail.com>
2024-05-14 18:32:06 +0400

    Translation rtl

--- resources/views/community_details/desktop/header.blade.php
+++ resources/views/community_details/desktop/header.blade.php
@@ -49,7 +49,7 @@
 <header class="">
     <div class="w-full bg-cover bg-bottom" style="height: 75vh; background-image: url({{ asset('front/communities/2.png') }}), url('{{ URL::asset('assets/img/img-error2.webp')}}');">
         {{-- <div class="w-full bg-cover bg-bottom" style="height: 75vh; background-image: url('https://mis.esnaad.com/uploads/communities/{{$id}}/header_image/{{$header_image}}'), url('{{ URL::asset('assets/img/img-error2.webp')}}');"> --}}
-        <div class="flex items-center justify-center h-full w-full bg-black bg-opacity-" style="padding-top: 10vh;">
+        <div class="flex items-center justify-center h-full w-full bg-black bg-opacity-30" style="padding-top: 10vh;">
             <div class="sm:container sm:mx-auto sm:px-4 sm:my-4 mx-auto  text-white">
                 <h1 class="text-4xl font-thin text-center">
                     @if($langSeg == 'ar')
