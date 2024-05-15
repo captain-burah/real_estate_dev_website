b20282e05c00e3e9b1d781453e8a72e512cc9a13
cptburah <cptburah@gmail.com>
2024-05-15 17:47:06 +0400

    project details translations

--- resources/views/development_details/mobile/buttons.blade.php
+++ resources/views/development_details/mobile/buttons.blade.php
@@ -23,7 +23,11 @@
 <div class="mx-0 px-0 ">
     <div class="mx-0">
         <button onclick="openModal('mymodalcentered-community-register')" class="w-full text-lg text-white border border-white py-4 px-2 rounded-0" type="button">
-            Register Your Interest!
+            @if($langSeg=='ar')
+            سجل اهتمامك
+            @else
+            Register Your Interest
+            @endif
         </button>
     </div>
 </div>
